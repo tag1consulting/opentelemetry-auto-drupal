b7fa9d61d3fd1b25ecda16fb132de64db6a8a27e
Fabian Franz <fabian@tag1consulting.com>
2025-01-28 18:50:34 +0100

    Fix url -> uri and set SPAN_CLIENT.

--- src/HttpClientRequestInstrumentation.php
+++ src/HttpClientRequestInstrumentation.php
@@ -5,6 +5,7 @@ namespace OpenTelemetry\Contrib\Instrumentation\Drupal;
 use GuzzleHttp\Client;
 use GuzzleHttp\Promise\Promise;
 use GuzzleHttp\Psr7\Response as GuzzleResponse;
+use OpenTelemetry\API\Trace\SpanKind;
 use OpenTelemetry\SemConv\TraceAttributes;
 
 class HttpClientRequestInstrumentation extends InstrumentationBase {
@@ -21,18 +22,19 @@ class HttpClientRequestInstrumentation extends InstrumentationBase {
   protected function registerInstrumentation(): void {
     $operations = [
       'requestAsync' => [
-        'preHandler' => function($spanBuilder, $object, array $params) {
-          $host = $params['url'];
-          if (filter_var($params['url'], FILTER_VALIDATE_URL)) {
-            $host = parse_url($params['url'], PHP_URL_HOST);
+        'preHandler' => function($spanBuilder, Client $client, array $namedParams) {
+          $host = $namedParams['uri'];
+          if (filter_var($namedParams['uri'], FILTER_VALIDATE_URL)) {
+            $host = parse_url($namedParams['uri'], PHP_URL_HOST);
           }
 
           $spanBuilder->setName($host)
-            ->setAttribute(TraceAttributes::HTTP_REQUEST_METHOD, $params['method'])
-            ->setAttribute(TraceAttributes::URL_FULL, $params['url'])
+            ->setSpanKind(SpanKind::KIND_CLIENT)
+            ->setAttribute(TraceAttributes::HTTP_REQUEST_METHOD, $namedParams['method'])
+            ->setAttribute(TraceAttributes::URL_FULL, $namedParams['uri'])
             ->setAttribute(TraceAttributes::SERVER_ADDRESS, $host);
         },
-        'postHandler' => function($span, $object, array $params, $response) {
+        'postHandler' => function($span, Client $client, array $namedParams, $response) {
           if ($response instanceof Promise) {
             $response = $response->wait();
           }
