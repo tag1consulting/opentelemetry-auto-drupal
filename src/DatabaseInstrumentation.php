e067245d8fe49ecddc6085a4b86b08d4b8b590f0
Fabian Franz <fabian@tag1consulting.com>
2025-01-28 18:29:04 +0100

    AI refactor of DatabaseInstrumentation

--- src/DatabaseInstrumentation.php
+++ src/DatabaseInstrumentation.php
@@ -3,57 +3,42 @@
 namespace OpenTelemetry\Contrib\Instrumentation\Drupal;
 
 use Drupal\Core\Database\Connection;
-use OpenTelemetry\API\Instrumentation\CachedInstrumentation;
 use OpenTelemetry\API\Trace\SpanKind;
-use OpenTelemetry\Context\Context;
 use OpenTelemetry\SemConv\TraceAttributes;
-use function \OpenTelemetry\Instrumentation\hook;
 
 class DatabaseInstrumentation extends InstrumentationBase {
-
+  protected const CLASSNAME = Connection::class;
   public const DB_VARIABLES = 'db.variables';
 
   public static function register(): void {
-
-    $instrumentation = new CachedInstrumentation('io.opentelemetry.contrib.php.drupal');
-
-    hook(
-      Connection::class,
-      'query',
-      static::preClosure($instrumentation),
-      static::postClosure()
+    static::create(
+      name: 'io.opentelemetry.contrib.php.drupal',
+      prefix: 'drupal.database',
+      className: static::CLASSNAME
     );
-
   }
 
-  /**
-   * @param \OpenTelemetry\API\Instrumentation\CachedInstrumentation $instrumentation
-   *
-   * @return \Closure
-   */
-  public static function preClosure(CachedInstrumentation $instrumentation): \Closure {
-    return static function (Connection $connection, array $params, string $class, string $function, ?string $filename, ?int $lineno) use ($instrumentation) {
-      $parent = Context::getCurrent();
+  protected function registerInstrumentation(): void {
+    $operations = [
+      'query' => [
+        'params' => [],
+        'preHandler' => function($spanBuilder, $object, array $params) {
+          $spanBuilder
+            ->setSpanKind(SpanKind::KIND_CLIENT)
+            ->setAttribute(TraceAttributes::DB_SYSTEM, 'mariadb')
+            ->setAttribute(TraceAttributes::DB_STATEMENT, $params[0]);
 
-      /** @var \OpenTelemetry\API\Trace\SpanBuilderInterface $span */
-      $span = $instrumentation->tracer()->spanBuilder('database::query')
-        ->setParent($parent)
-        ->setSpanKind(SpanKind::KIND_CLIENT)
-        ->setAttribute(TraceAttributes::CODE_FUNCTION, $function)
-        ->setAttribute(TraceAttributes::CODE_NAMESPACE, $class)
-        ->setAttribute(TraceAttributes::CODE_FILEPATH, $filename)
-        ->setAttribute(TraceAttributes::CODE_LINENO, $lineno)
-        ->setAttribute(TraceAttributes::DB_SYSTEM, 'mariadb')
-        ->setAttribute(TraceAttributes::DB_STATEMENT, $params[0]);
-      if (isset($params[1]) === TRUE) {
-        $cleanVariables = array_map(static fn ($value) => is_array($value) ? json_encode($value) : (string) $value, $params[1]);
-        $span->setAttribute(self::DB_VARIABLES, $cleanVariables);
-      }
-      $span = $span->startSpan();
+          if (isset($params[1]) === TRUE) {
+            $cleanVariables = array_map(
+              static fn ($value) => is_array($value) ? json_encode($value) : (string) $value,
+              $params[1]
+            );
+            $spanBuilder->setAttribute(self::DB_VARIABLES, $cleanVariables);
+          }
+        }
+      ]
+    ];
 
-      Context::storage()
-        ->attach($span->storeInContext($parent));
-    };
+    $this->registerOperations($operations);
   }
-
 }
